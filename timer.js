65bbab74f1367c070aa9073cd3d823658bc8378e
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-03-28 10:48:57 -0400

    yes

--- timer.js
+++ timer.js
@@ -1,1 +1,15 @@
+var delayInMilliseconds = 1000;
 
+function start() {
+  timer();
+}
+
+
+function timer() {
+    var x = 31;
+    var x = x-1;
+    document.getElementById('timer').innerHTML = x + "s";
+  if (x !== 0) {
+    timer();
+  }
+}, delayInMilliseconds);
