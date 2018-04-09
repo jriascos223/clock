5d85b574f4aafc7ba31ee93f6eef8db0232b694a
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-04-09 10:40:49 -0400

    help

--- timer.js
+++ timer.js
@@ -1,5 +1,7 @@
 var delay = 1000;
-var x = 11;
+var x = document.getElementById("input").value;
+// x = 11
+console.log(x);
 
 function start() {
   var btn = document.getElementById("button"); btn.disabled = true;
@@ -10,7 +12,7 @@ function start() {
 function timer() {
 
     x = x-1;
-    document.getElementById('timer').innerHTML = x + "s";
+    document.getElementById('input').value = x;
     if (x==0) {
       clearInterval(myVar);
       var btn = document.getElementById("button"); btn.disabled = false;
