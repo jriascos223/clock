2b54f195bc6f15efad328c8869089d37785e2c1b
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-04-11 09:59:07 -0400

    good enough

--- timer.js
+++ timer.js
@@ -1,21 +1,23 @@
 var delay = 1000;
 // var x = document.getElementById("input").value;
-x = 11
-console.log(x);
+var x = 30;
+// console.log(x);
+
 
 function start() {
+  var audio = new Audio('sounds/alarm.mp3');
   var btn = document.getElementById("button"); btn.disabled = true;
+  x = +prompt('Enter time amout');
+  document.getElementById('timer').innerHTML = x;
   var myVar = setInterval(timer, 1000);
-
 
 
 function timer() {
 
     x = x-1;
-    document.getElementById('input').value = x;
+    document.getElementById('timer').innerHTML = x;
     if (x==0) {
       clearInterval(myVar);
-      var audio = new Audio('sounds/alarm.mp3');
       audio.play();
       var btn = document.getElementById("button"); btn.disabled = false;
     }
