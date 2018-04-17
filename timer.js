03a7a954cb61f18f30e7a1a8cea9c9b2ec86434b
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-04-17 13:23:39 -0400

    textbox working

--- timer.js
+++ timer.js
@@ -1,26 +1,30 @@
 var delay = 1000;
-// var x = document.getElementById("input").value;
-var time1 = 30;
+// var time1 = document.getElementById("input").value;
+var time1 = parseInt(time1);
+// var time1 = 30;
 var time2 = 0;
 var time3 = 0;
-// console.log(x);
+console.log(time1);
 
 
 function start() {
+ time1 = document.getElementById("timer").value;
+   time1 = parseInt(time1);
+  console.log(time1);
   var audio = new Audio('sounds/alarm.mp3');
   var btn = document.getElementById("button");
   btn.disabled = true;
-  x = +prompt('Enter time amout');
-  document.getElementById('timer').innerHTML = x;
+  // time1 = +prompt('Enter time amout');
+  document.getElementById('timer').value = time1;
   var myVar = setInterval(timer, 1000);
 
 
   function timer() {
 
     time1 = time1 - 1;
-    document.getElementById('timer').innerHTML = x;
+    document.getElementById('timer').value = time1;
 
-    if (x == 0) {
+    if (time1 == 0) {
       clearInterval(myVar);
       audio.play();
       var btn = document.getElementById("button");
