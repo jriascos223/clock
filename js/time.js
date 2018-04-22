3e9cc33375457dd7ec5a5cfa0ce93b3c6d4c31b3
Joseph Riascos <josephriascos@gmail.com>
2018-04-21 20:31:20 -0400

    working alarm clock with different sounds, in the process of adding nav bar

--- js/time.js
+++ js/time.js
@@ -1,41 +1,28 @@
 var timeouts = [];
 var currentZone;
 window.onload = function calcTime() {
-  //in order to get location we will make an ajax request
-  //hope to soon include the ability to get correct time (not system time) from a remote server
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', "https://ipinfo.io/json", true);
-  xhr.send();
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var response = JSON.parse(xhr.responseText);
-      document.getElementById('message').innerHTML = "The current time in " + response.city + ", " + response.region + " in the " + response.country + " is...";
-    }
-  };
   displayTime();
-
 };
 
 function displayTime(diff, clear, location) {
-  document.getElementById('message').innerHTML = "The current time in " + location + " is...";
+  document.getElementById('message').innerHTML = "The current time in your location is...";
   if (clear == true) {
-      //literally searches all timeouts in the file and exterminates them
+    //literally searches all timeouts in the file and exterminates them
     //ended up finding this online cause I just couldn't clear the timeouts I had
-    let id = window.setTimeout(() => {}, 0);
+    let id = window.setTimeout(() => { }, 0);
     console.log(id);
     while (id) {
       window.clearTimeout(id);
       id--;
     }
 
-    id = window.setInterval(() => {}, 0);
+    id = window.setInterval(() => { }, 0);
     console.log(id);
     while (id) {
       window.clearInterval(id);
       id--;
     }
-    }
+  }
   timeouts.push(setTimeout(function getTime() {
     if (diff == undefined) {
 
@@ -45,7 +32,7 @@ function displayTime(diff, clear, location) {
       var outputHour = hour;
       var relativeNoon = "AM";
       if (outputHour > 12) {
-        outputHour = outputHour -  12;
+        outputHour = outputHour - 12;
         relativeNoon = "PM";
       }
       if (outputHour == 0) {
@@ -67,7 +54,7 @@ function displayTime(diff, clear, location) {
       var outputHour = hour + diff;
       var relativeNoon = "AM";
       if (outputHour > 12) {
-        outputHour = outputHour -  12;
+        outputHour = outputHour - 12;
         relativeNoon = "PM";
       }
       if (outputHour < 0) {
